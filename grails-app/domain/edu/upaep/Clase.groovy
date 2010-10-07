4d2b6e22ec0ff973255fce1e1fbe04f4b3a03a4e
hectorrasgado <kuroz@.(none)>
2010-10-07 12:09:23 -0500

    clase de dominio agregada

---
+++ grails-app/domain/edu/upaep/Clase.groovy
@@ -0,0 +1,7 @@
+package edu.upaep
+
+class Clase {
+
+    static constraints = {
+    }
+}
---
+++ test/unit/edu/upaep/ClaseTests.groovy
@@ -0,0 +1,17 @@
+package edu.upaep
+
+import grails.test.*
+
+class ClaseTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
