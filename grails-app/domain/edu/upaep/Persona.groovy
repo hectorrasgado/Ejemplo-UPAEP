6f26dd5102526c7330b4658a11526b5d477594bb
neodevelop <neodevelop@gmail.com>
2010-10-07 10:26:42 -0500

    Clase de dominio persona creado...

---
+++ grails-app/domain/edu/upaep/Persona.groovy
@@ -0,0 +1,7 @@
+package edu.upaep
+
+class Persona {
+
+    static constraints = {
+    }
+}
---
+++ test/unit/edu/upaep/PersonaTests.groovy
@@ -0,0 +1,17 @@
+package edu.upaep
+
+import grails.test.*
+
+class PersonaTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
